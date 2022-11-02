baf312405f52bbb6716147ba28a498219b05356e
kozhanovalexey <alekseikozhanov@gmail.com>
2022-11-03 02:03:46 +0300

    second commit (after figuring out why did not push the first time)

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public float spawnPeriod;
+    public float cubeSpeed;
+    public float cubeDistanceDestroy;
+    public GameObject cubesParent;
+    private int maxSpawn = 100;
+    private int nowSpawned = 0;
+    DateTime lastSpawn;
+    public GameObject cubePrefab;
+
+    private void Start()
+    {
+        SetSpeedToCubes(cubeSpeed);
+        lastSpawn = DateTime.Now;
+    }
+
+    void Update()
+    {
+        // Destroy all outraged cubes.
+        for (int i = 0; i < cubesParent.transform.childCount; i++)
+        {
+            Transform cubeTransform = cubesParent.transform.GetChild(i);
+            Vector3 cubePosition = cubeTransform.position;
+            if (cubePosition.z > cubeDistanceDestroy)
+            {
+                Destroy(cubeTransform.gameObject);
+                nowSpawned--;
+            }
+        }
+
+        if (lastSpawn.AddSeconds(spawnPeriod) < DateTime.Now)
+        {
+            lastSpawn = DateTime.Now;
+            if (nowSpawned < maxSpawn)
+            {
+                GameObject newCube = Instantiate(cubePrefab, cubesParent.transform);
+                newCube.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, cubeSpeed);
+                nowSpawned++;
+            }
+        }
+    }
+
+    public void SetSpeedToCubes(float neededSpeed)
+    {
+        for (int i = 0; i < cubesParent.transform.childCount; i++)
+        {
+            Transform cubeTransform = cubesParent.transform.GetChild(i);
+            Rigidbody rb = cubeTransform.gameObject.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(0, 0, neededSpeed);
+        }
+    }
+
+    public void SetSpeedToCubesString(string speed)
+    {
+        cubeSpeed = float.Parse(speed);
+        SetSpeedToCubes(cubeSpeed);
+    }
+
+    public void SetSpawnPeriodString(string period)
+    {
+        spawnPeriod = float.Parse(period);
+    }
+
+    public void SetCubeDistanceDestroyString(string distance)
+    {
+        cubeDistanceDestroy = float.Parse(distance);
+    }
+}
---
+++ Assets/Scripts/InputsManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputsManager : MonoBehaviour
+{
+    public GameController controller;
+    public InputField inputField;
+
+    public void spawnTimeToController()
+    {
+        controller.SetSpawnPeriodString(inputField.text);
+    }
+
+    public void speedToController()
+    {
+        controller.SetSpeedToCubesString(inputField.text);
+    }
+
+    public void distanceToController()
+    {
+        controller.SetCubeDistanceDestroyString(inputField.text);
+    }
+}
